c769a7b6073de77d7752f9df1b1ea780c787aa5c
jinwapp <jinwapp@gmail.com>
2022-08-23 02:48:16 +0900

    first

---
+++ src/main/java/com/example/homeworkgithub/HomeworkGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkGithubApplication.class, args);
+	}
+
+}
